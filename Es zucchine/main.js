4e8fa145610fabba053cc0f28fea0200d17da925
DavideL90 <davide.lecci@icloud.com>
2018-03-21 14:37:47 +0100

    impostato ciclo che riempie automaticamente un array con gli oggetti zucchine

--- Es zucchine/main.js
+++ Es zucchine/main.js
@@ -8,9 +8,13 @@ zucchineArray = [];
 //Faccio un ciclo per riempire l'array degli oggetti
 for (var i = 0; i < arrayObjNames.length; i++) {
     arrayObjNames[i] = {
-    "varietà" : prompt("Inserisci la varietà:"),
-    "peso" : 200,
-    "lunghezza" : 20
+      "varietà" : "varietà1",
+      "peso" : 30,
+      "lunghezza" : 20
+    //La versione commentata sotto l'ho messa solo per vedere se funzionava. Per fare più veloce imposto dati già prestabiliti
+    // "varietà" : prompt("Inserisci la varietà della " + (i+1) + " zucchina in elenco:"),
+    // "peso" : parseInt(prompt("Inserisci il peso della " + (i+1) + " zucchina in elenco")),
+    // "lunghezza" : prompt("Inserisci la lunghezza " + (i+1) + " zucchina in elenco")
   }
   zucchineArray.push(arrayObjNames[i]);
 }
